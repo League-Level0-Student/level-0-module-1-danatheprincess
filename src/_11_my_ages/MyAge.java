62b2920e439786861c2f812e22105d84d40adc5e
i-lab user <i-labuser@SDPL88539-iMacProLO.local>
2020-01-21 17:58:44 -0800

    banana split

---
+++ src/_11_my_ages/MyAge.java
@@ -0,0 +1,12 @@
+package _11_my_ages;
+
+public class MyAge {
+	
+public static void main(String[] args) {
+	
+
+	for(int d=0;d<13;d++) {
+		System.out.println(d);
+	}
+}
+}
---
+++ src/_13_bananas_4eva/bananas_4eva/bananas_4eva_pde/bananas_4eva_pde.pde
@@ -0,0 +1,3 @@
+for(int b=0;b<1000;b++){
+  println("banana");
+}
