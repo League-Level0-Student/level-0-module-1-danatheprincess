38dea493e3208492cb4c2dabb413f3d04383b8de
i-lab user <i-labuser@SDPL88539-iMacProLO.local>
2020-01-21 17:59:36 -0800

    finish

--- src/Badger.java
+++ src/Badger.java
@@ -1,13 +1,12 @@
 
 public class Badger {
-public static void main(String[] args) {
-	for(int b=0;b<12;b++ ) {
-		System.out.println("badger");
-	}
-	for(int m=0;m<2;m++) {
-	System.out.println("mushroom");
-	
-	}
+	public static void main(String[] args) {
+		for (int b = 0; b < 12; b++) {
+			System.out.println("badger");
+		}
+		for (int m = 0; m < 2; m++) {
+			System.out.println("mushroom");
+
+		}
 	}
 }
-
